f65e2074f5b74ec63f5b38398fd880cef6b10737
Lucas Regil <2026regil.lucas@fcboe>
2022-11-07 14:45:20 -0500

    input programs

---
+++ u/name.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+	char name[30];
+	printf("character name: ");
+	scanf("%s", &name);
+	printf("your name is %s", name);
+	return 0;
+}
---
+++ u/user_inteface.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("enter a number: ");
+	scanf("%i", &a);
+	printf("you typed %i\n", a);
+	return 0;
+}
